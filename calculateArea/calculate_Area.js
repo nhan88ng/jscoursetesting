70c67352ccaf556036aa68173f8f77a0a6ff395c
Nhan Ng <nhan88ng@gmail.com>
2025-08-22 05:43:38 -0400

    Add groceryTracker Feature

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,5 +1,4 @@
-let length;
-let width;
+let length, width;
 
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
@@ -8,4 +7,16 @@ function calculateArea() {
     let area = length * width;
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
+
+let grocery1, grocery2, grocery3;
+
+function groceryTracker(){
+    grocery1 = parseFloat(document.getElementById('grocery-1').value);
+    grocery2 = parseFloat(document.getElementById('grocery-2').value);
+    grocery3 = parseFloat(document.getElementById('grocery-3').value);
+
+    let total = grocery1 + grocery2 + grocery3;
+
+    document.getElementById('totalAmount').innerText = `The total amount is: ${total}`;
 }
